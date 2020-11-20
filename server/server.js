56ef23b6fe5c1611dc17a527c59976d34636f012
Wai Fai Lau <wlau8088@gmail.com>
2020-11-20 11:31:29 -0500

    webpack

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const { dirname } = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/index.html'));
+});
+
+module.exports = app.listen(port, () =>
+  console.log(`server is listening at ${port}`)
+);
